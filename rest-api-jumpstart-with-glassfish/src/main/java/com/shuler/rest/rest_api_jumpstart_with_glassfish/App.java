a39f064950cce9a8fe1367358ba439157fe3fd57
gbshuler <brad.shuler@me.com>
2017-08-30 18:00:46 -0500

    Glassfish Maven Project for Eclipse

---
+++ rest-api-jumpstart-with-glassfish/src/main/java/com/shuler/rest/rest_api_jumpstart_with_glassfish/App.java
@@ -0,0 +1,13 @@
+package com.shuler.rest.rest_api_jumpstart_with_glassfish;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ rest-api-jumpstart-with-glassfish/src/test/java/com/shuler/rest/rest_api_jumpstart_with_glassfish/AppTest.java
@@ -0,0 +1,38 @@
+package com.shuler.rest.rest_api_jumpstart_with_glassfish;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
